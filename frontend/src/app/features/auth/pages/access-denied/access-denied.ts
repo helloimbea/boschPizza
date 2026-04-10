0c5236f1e40d003e090ec5bc91c0a2a5cf1205b0
Beatriz <beatrizheimann@gmail.com>
2026-04-10 11:48:10 -0300

    pagina de erro 404

--- frontend/src/app/features/auth/pages/access-denied/access-denied.ts
+++ frontend/src/app/features/auth/pages/access-denied/access-denied.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-access-denied',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './access-denied.html',
   styleUrl: './access-denied.css',
 })
